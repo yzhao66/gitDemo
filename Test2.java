1ae78080f9e0d462a37b9bf2fd10ebe5c7f778b4
yangzhuohao <cttclouds@163.com>
2023-02-04 13:57:22 +0800

    add Test2

---
+++ Test2.java
@@ -0,0 +1,23 @@
+/**
+ * @author zoh66
+ * @Description
+ * @create 2023-02-04 1:56 PM
+ */
+public class Test2 {
+
+
+    @org.junit.Test
+    public void testA(){
+        System.out.println("AAAAA");
+    }
+
+    @org.junit.Test
+    public void testB(){
+        System.out.println("BBBBBBB");
+    }
+
+    @org.junit.Test
+    public void testC(){
+        System.out.println("BBBBBBB");
+    }
+}
