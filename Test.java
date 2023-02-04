560f6de0ce835fcc28647defb505668de0a4907b
yangzhuohao <cttclouds@163.com>
2023-02-04 13:10:46 +0800

    add TESTA

---
+++ Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author zoh66
+ * @Description
+ * @create 2023-02-04 1:09 PM
+ */
+public class Test {
+
+    @org.junit.Test
+    public void testA(){
+
+        System.out.println("AAAAA");
+    }
+}
