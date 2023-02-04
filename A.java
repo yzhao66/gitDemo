cc1ed6e3477feca2788b1cd2d2a2458a4cdeee11
yangzhuohao <cttclouds@163.com>
2023-02-04 12:53:18 +0800

    add

---
+++ A.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+  *@Description      
+  *@author          zoh66
+  *@create          2023-02-04 12:52 PM
+  */  
+public class A {
+    @Test
+    public void tes1(){
+        System.out.println("aaa");
+    }
+}
